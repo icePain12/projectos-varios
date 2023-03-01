1961a33eac0a5d706bbc7057d41d298fef2607b5
Luis David Bocanegra <54543119+icePain12@users.noreply.github.com>
2023-02-28 21:02:27 -0600

    Add files via upload

---
+++ practicando_Javascript/ejemplo_localstorage/buscar_termino_con_localstorage.js
@@ -0,0 +1,27 @@
+// Obtener el valor del campo de búsqueda
+var mi_texto = document.getElementById("search").value;
+
+// Obtener la lista de búsquedas almacenadas en localStorage
+var busquedas = JSON.parse(localStorage.getItem("busquedas")) || [];
+
+// Buscar si ya se ha realizado una búsqueda anteriormente
+var terminoEncontrado = false;
+for (var i = 0; i < busquedas.length; i++) {
+  if (busquedas[i].termino === mi_texto) {
+    terminoEncontrado = true;
+    break;
+  }
+}
+
+// Si el término no ha sido buscado anteriormente, agregarlo a la lista
+if (!terminoEncontrado) {
+  busquedas.push({ termino: mi_texto });
+  localStorage.setItem("busquedas", JSON.stringify(busquedas));
+}
+
+// Mostrar la lista de búsquedas en un div con clase "element"
+var div = document.querySelector(".element");
+div.innerHTML = "";
+for (var i = 0; i < busquedas.length; i++) {
+  div.innerHTML += busquedas[i].termino + "<br>";
+}
